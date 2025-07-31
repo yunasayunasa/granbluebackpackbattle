4845857bcc5b0bc43930628de19cfbac9130240d
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-31 17:11:49 +0900

    ぴ

--- src/core/BloomPipeline.js
+++ src/core/BloomPipeline.js
@@ -1,27 +1,25 @@
 export default class BloomPipeline extends Phaser.Renderer.WebGL.Pipelines.PostFXPipeline {
     constructor(game) {
-        // ★★★ この super() の呼び出し方を修正 ★★★
         super({
             game: game,
-            // fragShaderプロパティではなく、シェーダーのキーを直接渡す
-            // Phaserが内部で game.cache.shader.get('Bloom') を自動的に行ってくれる
-            fragShaderKey: 'Bloom' 
+            fragShader: game.cache.shader.get('Bloom')
+            // ★ fragShaderKeyがダメなら、元の game.cache.shader.get に戻します
+            //   エラーの原因は onBoot だったため、こちらは問題ないはずです
         });
         
         this.bloomRadius = 1.0;
         this.bloomIntensity = 1.0;
     }
 
+    // ★ onBootは削除
+
+    // onPreRenderは、毎フレーム描画の直前に呼ばれる
     onPreRender() {
+        // ★★★ setTextureSize の代わりに set2f を使う ★★★
+        this.set2f('uTextureSize', this.renderer.width, this.renderer.height);
+
+        // 他のuniform変数の設定は変更なし
         this.set1f('uBloomRadius', this.bloomRadius);
         this.set1f('uBloomIntensity', this.bloomIntensity);
-        this.set2f('uTextureSize', this.renderer.width, this.renderer.height);
-    }
-    
-    // ★★★ onBootメソッドを追加 ★★★
-    // パイプラインがWebGLコンテキストにバインドされる際に一度だけ呼ばれる
-    onBoot() {
-        // テクスチャサイズをここで設定するのがより安全
-        this.setTextureSize(this.renderer.width, this.renderer.height);
     }
 }
