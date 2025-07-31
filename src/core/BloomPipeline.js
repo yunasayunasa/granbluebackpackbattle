ba24a7a2ea68d3fcc2cdc0e2414d8349841b0d17
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-31 17:07:04 +0900

    pipeline

--- src/core/BloomPipeline.js
+++ src/core/BloomPipeline.js
@@ -1,9 +1,13 @@
 export default class BloomPipeline extends Phaser.Renderer.WebGL.Pipelines.PostFXPipeline {
     constructor(game) {
+        // ★★★ この super() の呼び出し方を修正 ★★★
         super({
             game: game,
-            fragShader: game.cache.shader.get('Bloom')
+            // fragShaderプロパティではなく、シェーダーのキーを直接渡す
+            // Phaserが内部で game.cache.shader.get('Bloom') を自動的に行ってくれる
+            fragShaderKey: 'Bloom' 
         });
+        
         this.bloomRadius = 1.0;
         this.bloomIntensity = 1.0;
     }
@@ -12,5 +16,12 @@ export default class BloomPipeline extends Phaser.Renderer.WebGL.Pipelines.PostF
         this.set1f('uBloomRadius', this.bloomRadius);
         this.set1f('uBloomIntensity', this.bloomIntensity);
         this.set2f('uTextureSize', this.renderer.width, this.renderer.height);
+    }
+    
+    // ★★★ onBootメソッドを追加 ★★★
+    // パイプラインがWebGLコンテキストにバインドされる際に一度だけ呼ばれる
+    onBoot() {
+        // テクスチャサイズをここで設定するのがより安全
+        this.setTextureSize(this.renderer.width, this.renderer.height);
     }
 }
