a5445830db0a7d0f6100ac3165315586d32535e1
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-31 15:08:41 +0900

    ぴぺ

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import RewardScene from './scenes/RewardScene.js';
 import GameOverScene from './scenes/GameOverScene.js';
 import GameClearScene from './scenes/GameClearScene.js';
 import ScoreScene from './scenes/ScoreScene.js';
+import BloomPipeline from './core/BloomPipeline.js'; 
 const config = {
     type: Phaser.AUTO,
     scale: {
