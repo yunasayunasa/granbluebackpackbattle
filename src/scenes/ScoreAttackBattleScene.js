c6f3ad660c04922be7150b51787387cd3a158f05
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-08-01 17:33:06 +0900

    rifact

---
+++ src/scenes/ScoreAttackBattleScene.js
@@ -0,0 +1,10 @@
+// scenes/ScoreAttackBattleScene.js
+
+import BattleScene from './BattleScene.js';
+
+export default class ScoreAttackBattleScene extends BattleScene {
+    constructor() {
+        // スコアアタック用のユニークなキーを設定
+        super('ScoreAttackBattleScene');
+    }
+}
