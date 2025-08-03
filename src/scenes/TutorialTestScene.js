aa1be696d3b280a65b1dd55b943579acbec28e1a
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-08-03 11:41:33 +0900

    Update TutorialTestScene.js

--- src/scenes/TutorialTestScene.js
+++ src/scenes/TutorialTestScene.js
@@ -8,6 +8,12 @@ export default class TutorialTestScene extends Phaser.Scene {
             console.log("四角形がクリックされました！");
             this.stateManager.setSF('test_flag', 'clicked');
         });
+        this.time.delayedCall(100, () => { // 描画が安定するのを少し待つ
+            this.scene.get('SystemScene').events.emit('request-overlay', { 
+                from: this.scene.key,
+                scenario: 'tutorial_test.ks'
+            });
+        });
         this.events.emit('scene-ready');
     }
 }
